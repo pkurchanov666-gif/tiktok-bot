b473810939b4ee9fd44845fd2a25a2ec3daebb78
pkurchanov666-gif <p.kurchanov666@gmail.com>
2026-05-01 22:34:26 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -151,12 +151,13 @@ def main():
     app.add_handler(CallbackQueryHandler(ai_photoshoot, pattern="^ai_photoshoot$"))
     app.add_handler(CallbackQueryHandler(regen_photo, pattern="^regen_"))
     app.add_handler(CallbackQueryHandler(send_buffer_handler, pattern="^send_.*buffer$"))
-    print("Бот погнал!")
+        print("Бот погнал!")
     app.run_webhook(
-    listen="0.0.0.0",
-    port=int(os.environ.get("PORT", 8080)),
-    url_path=BOT_TOKEN,
-    webhook_url=f"https://tiktok-bot-production-4530.up.railway.app/{BOT_TOKEN}",
+        listen="0.0.0.0",
+        port=int(os.environ.get("PORT", 8080)),
+        url_path=BOT_TOKEN,
+        webhook_url=f"https://tiktok-bot-production-4530.up.railway.app/{BOT_TOKEN}",
+    )
 )
 
 if __name__ == "__main__":
