4f8b9d4387c86ee6644c4009d8107b2c850c1cdf
pkurchanov666-gif <p.kurchanov666@gmail.com>
2026-05-07 17:32:07 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,28 @@
-from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Application, CommandHandler, CallbackQueryHandler, ContextTypes
 from config import BOT_TOKEN
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Бот жив ✅")
 
-async def error_handler(update: object, context: ContextTypes.DEFAULT_TYPE):
-    print("ОШИБКА:", context.error)
+    keyboard = InlineKeyboardMarkup([
+        [InlineKeyboardButton("📸 AI Фотосессия", callback_data="ai")]
+    ])
+
+    await update.message.reply_text(
+        "Выберите режим:",
+        reply_markup=keyboard
+    )
+
+async def ai_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    await query.answer()
+    await query.edit_message_text("Кнопка работает ✅")
 
 def main():
     app = Application.builder().token(BOT_TOKEN).build()
+
     app.add_handler(CommandHandler("start", start))
-    app.add_error_handler(error_handler)
+    app.add_handler(CallbackQueryHandler(ai_handler, pattern="^ai$"))
 
     print("Бот погнал!")
     app.run_polling()
